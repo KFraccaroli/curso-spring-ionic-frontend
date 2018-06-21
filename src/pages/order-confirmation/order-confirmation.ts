e03cbba979360e6644a146153712d16434f86dd8
Kaue Fraccaroli <kaue_fraccaroli@yahoo.com.br>
2018-06-20 22:07:24 -0300

    salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { CartService } from '../../services/domain/cart.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 @IonicPage()
 @Component({
@@ -23,7 +24,8 @@ export class OrderConfirmationPage {
     public navCtrl: NavController, 
     public navParams: NavParams,
     public cartService : CartService,
-    public clienteService : ClienteService) {
+    public clienteService : ClienteService,
+    public pedidoService : PedidoService) {
     
     this.pedido = this.navParams.get('pedido');
   }
@@ -41,7 +43,16 @@ export class OrderConfirmationPage {
   }
 
   checkout(){
-
+    this.pedidoService.insert(this.pedido)
+      .subscribe(response =>{
+        console.log(response.headers.get('location'));
+        this.cartService.createOrClearCart();
+      },
+      error => {
+        if(error.status == 403){
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
   }
 
   private findEndereco(id: string, list : EnderecoDTO[]) : EnderecoDTO{
@@ -51,6 +62,10 @@ export class OrderConfirmationPage {
 
   total(){
     return this.cartService.total();
+  }
+
+  back(){
+    this.navCtrl.setRoot('CartPage');
   }
 
 }
