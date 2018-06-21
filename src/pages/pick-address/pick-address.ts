8f167f7a76069860695070a04b3834bd6a5b90a7
Kaue Fraccaroli <kaue_fraccaroli@yahoo.com.br>
2018-06-20 21:38:32 -0300

    tela de escolha de pagamento

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -60,7 +60,7 @@ export class PickAddressPage {
 
   nextPage(item : EnderecoDTO){
     this.pedido.enderecoDeEntrega = {id: item.id};
-    console.log(this.pedido);
+    this.navCtrl.setRoot('PaymentPage',{pedido: this.pedido});
   }
 
 }
